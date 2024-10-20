890f9f98cf4a9684b78d8f8bb6c7a7ff5d55eb43
sakibur.rahman.cse <sakibur.rahman.cse@gmail.com>
2024-10-20 22:15:58 +0600

    upd

--- Common/EmailSettings.cs
+++ Common/EmailSettings.cs
@@ -6,8 +6,7 @@
         public string MailFromAddress = "sakibur.rahman.cse@gmail.com";
         public bool UseSsl = true;
         public string Username = "sakibur.rahman.cse@gmail.com";
-        //public string Password = "laydleksjeddhzqn";
-        public string Password = "kerksdaxnunmcotc";
+        public string Password = "";
         public string ServerName = "smtp.gmail.com";
         public int ServerPort = 587;
         public bool WriteAsFile = false;
